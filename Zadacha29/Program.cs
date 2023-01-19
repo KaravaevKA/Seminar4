c20e13a4c2e8723acd11a7f287eba5166a778bd6
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-19 23:02:39 +0300

    Добавлено решение задачи 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу, которая задает массив из 8 элементов, и выводит их на экран
+int[] Task29 ()
+{
+    int size = 8;
+    int[] NewMassive = new int [8];
+    for (int i = 0; i<8; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        NewMassive[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    for (int i = 0; i<8; i++)
+    {
+        Console.Write(NewMassive[i] + " ");
+    }
+    return NewMassive;
+}
+int[] TaskArray = Task29();
+
