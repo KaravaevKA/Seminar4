324c5e96259bb05ace7553669f06d967e1688685
Kirill Karavaev <coffee.2016@yandex.ru>
2023-01-19 21:22:49 +0300

    Добавлено решение задачи 27

--- Zadacha27/Program.cs
+++ Zadacha27/Program.cs
@@ -1,26 +1,20 @@
 ﻿//Программа принимает на ввод число и выдает сумму его цифр
-// void IndexSum(string Number)
-// {
-//     int sum =0;
-//     for (int i=0; i<Number.Length; i++)
-//     {
-//         sum = sum + Number[i];
-//         Console.WriteLine(sum);
-//     }
-//     Console.WriteLine($"Сумма цифр числа {Number} равна {sum}");
-// }
-// Console.WriteLine("Введите число");
-// string A =Console.ReadLine();
-// IndexSum(A);
 Console.WriteLine("Введите число");
-string A =Console.ReadLine();
-int count =A.Length;
-Console.WriteLine(count);
-int sum = 0;
-int i =0;
-while (i<count)
+int num =Convert.ToInt32(Console.ReadLine());
+void IndexSum(int A)
 {
-    sum +=A[i];
-    i++;
+    int sum = 0;
+    int i = 1;
+    while(A != 0)
+    {
+        while(i<A)
+        {
+            int b = A / i % 10;
+            sum = sum + b;
+            i = i*10;
+        }
+        Console.WriteLine(sum);
+        break;
+    }
 }
-Console.WriteLine(sum);
+IndexSum(num);
